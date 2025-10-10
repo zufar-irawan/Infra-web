b7c85ffd5aafe63f55caae21c8d658a0ed16d4f3
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-10-10 14:09:50 +0700

    Search & Filter tugas

---
+++ frontend/src/app/api/subject/route.ts
@@ -0,0 +1,23 @@
+import {cookies} from "next/headers";
+import api from "@/app/lib/api";
+import {NextResponse} from "next/server";
+
+export async function GET() {
+    const cookieStore = cookies()
+    // @ts-ignore
+    const token = cookieStore.get('secure-auth-token')?.value;
+
+    try {
+        const res = await api.get("/lms/subjects", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if(res.status === 200) {
+            return NextResponse.json(res.data)
+        }
+    } catch (error) {
+        return NextResponse.json({ error: error }, {status: 500})
+    }
+}
