af3f8045d55d617585bf7f96a5b553a16dee2e54
アネザ <anezacode@proton.me>
2025-10-16 15:09:24 +0700

    base

---
+++ frontend/src/app/edu/perangkat/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import DashHeader from "@/app/components/DashHeader";
+import { useEduData } from "@/app/edu/context";
+
+export default function Perangkat() {
+    const { user, student } = useEduData();
+
+    return (
+        <>
+            {user?.role === 'admin' && (
+                <div className="overflow-y-auto min-h-screen">
+                    <DashHeader user={user} student={student} />
+
+                    <div className="w-full p-4 flex flex-col gap-4">
+                        {/* // */}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
