ab4ce0b4dca80d218ec63d8d8310d32f00d35dc1
Dav1naa <davinavinsky@gmail.com>
2025-10-20 20:13:07 +0700

    cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*.smkprestasiprima.sch.id'],
 
     'allowed_origins_patterns' => [],
 
